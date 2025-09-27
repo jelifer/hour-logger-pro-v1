91206dc833fbee1bfe74a8be45c1a0c247ec84a0
jelifer <71771903+jelifer@users.noreply.github.com>
2025-09-27 11:19:04 -0400

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div style={{ padding: '20px', fontFamily: 'sans-serif' }}>
+      <h1>Welcome to Hour Logger Pro!</h1>
+      <p>Your time tracking application is running.</p>
+    </div>
+  );
+}
